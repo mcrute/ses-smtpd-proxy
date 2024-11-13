47f653ef8623e555933cfe388dbffafd5cea3b69
Mike Crute <mike@crute.us>
2024-11-12 22:51:49 -0800

    Remove unnecessary Vault renewal logic
    The secret we're using is static so there's no need to continually renew
    it and the presence of renewal results in crashes due to the Fatalf that
    occurs when the Vault global max TTL is reached.

--- main.go
+++ main.go
@@ -117,31 +117,6 @@ func (e *Envelope) Close() error {
 	return err
 }
 
-func renewSecret(vc *api.Client, s *api.Secret) error {
-	w, err := vc.NewLifetimeWatcher(&api.LifetimeWatcherInput{Secret: s})
-	if err != nil {
-		return err
-	}
-	go w.Start()
-
-	go func() {
-		for {
-			select {
-			case err := <-w.DoneCh():
-				if err != nil {
-					credentialRenewalError.Inc()
-					log.Fatalf("Error renewing credential: %s", err)
-				}
-			case renewal := <-w.RenewCh():
-				credentialRenewalSuccess.Inc()
-				log.Printf("Successfully renewed: %#v", renewal)
-			}
-		}
-	}()
-
-	return nil
-}
-
 func getVaultSecret(path string) (credentials.Value, error) {
 	var r credentials.Value
 
@@ -159,12 +134,8 @@ func getVaultSecret(path string) (credentials.Value, error) {
 		if err != nil {
 			return r, fmt.Errorf("unable to initialize AppRole auth method: %w", err)
 		}
-		if loginSecret, err := vc.Auth().Login(context.Background(), appRoleAuth); err != nil {
+		if _, err := vc.Auth().Login(context.Background(), appRoleAuth); err != nil {
 			return r, fmt.Errorf("unable to login to AppRole auth method: %w", err)
-		} else {
-			if err := renewSecret(vc, loginSecret); err != nil {
-				return r, err
-			}
 		}
 	}
 
@@ -189,7 +160,7 @@ func getVaultSecret(path string) (credentials.Value, error) {
 	r.AccessKeyID = keyId.(string)
 	r.SecretAccessKey = secretKey.(string)
 
-	return r, renewSecret(vc, secret)
+	return r, nil
 }
 
 func makeSesClient(enableVault bool, vaultPath string) (*ses.SES, error) {
